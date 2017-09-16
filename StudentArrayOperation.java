241cd91ff2da026b9592b1e540d2975764dcfd08
Manjulanarva <31886195+Manjulanarva@users.noreply.github.com>
2017-09-16 14:33:18 +0530

    Add files via upload

---
+++ StudentArrayOperation.java
@@ -0,0 +1,26 @@
+import java.util.Date;
+public interface StudentArrayOperation {
+	
+void addFirst(Student student); 
+	
+	void addLast(Student student);
+	void add(Student student, int index);
+	void remove(int index);
+	void remove(Student student);
+	void removeFromIndex(int index);
+	void removeFromElement(Student student);
+	void removeToIndex(int index);
+	void removeToElement(Student student);
+	void bubbleSort();
+	Student[] getByBirthDate(Date date);
+	Student[] getBetweenBirthDates(Date firstDate, Date lastDate);
+	Student[] getNearBirthDate(Date date, int days);
+	int getCurrentAgeByDate(int indexOfStudent);
+	Student[] getStudentsByAge(int age);
+	Student[] getStudentsWithMaxAvgMark();
+	Student getNextStudent(Student student);
+	Student[] getStudents();
+	void setStudent(Student student, int index);
+	Student getStudent(int index);
+	void setStudents(Student[] students);
+}
